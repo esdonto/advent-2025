d7b9e975c8bc4b4057a242b91b134d60e13766ce
esdonto <lucasoliveirareis@hotmail.com>
2025-12-04 10:03:15 -0300

    day 4

---
+++ day4/pt2/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("error!")
+		panic(err)
+	}
+	defer file.Close()
+
+	//sum := 0
+
+	// Assembiling the matrix
+
+	reader := bufio.NewReader(file)
+	line, err := reader.ReadString('\n')
+
+	werehouse := [][]bool{}
+	werehouse = append(werehouse, make([]bool, len(line)+1))
+
+	for ; err == nil; line, err = reader.ReadString('\n') {
+		row := make([]bool, len(line)+1)
+
+		for i := range len(row) - 1 {
+			if line[i] == '@' {
+				row[i+1] = true
+			}
+		}
+
+		werehouse = append(werehouse, row)
+	}
+
+	werehouse = append(werehouse, make([]bool, len(werehouse[0])))
+
+	// Checking the matrix
+
+	count := 0
+
+	rollsRemoved := true
+	for rollsRemoved {
+		rollsRemoved = false
+
+		for i := 1; i < len(werehouse)-1; i++ {
+			for j := 1; j < len(werehouse[i])-1; j++ {
+				if werehouse[i][j] {
+					if count3x3(werehouse, i, j) < 5 {
+						count++
+						werehouse[i][j] = false // alg is not exactly the same, but it ends in the same result
+						rollsRemoved = true
+					}
+				}
+			}
+		}
+	}
+
+	println("count:", count)
+}
+
+func count3x3(werehouse [][]bool, row int, column int) int {
+	count := 0
+
+	for i := row - 1; i <= row+1; i++ {
+		for j := column - 1; j <= column+1; j++ {
+			if werehouse[i][j] {
+				count++
+			}
+		}
+	}
+
+	return count
+}
